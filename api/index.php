7faac7a38965f02a762f213cf41588dc7d21595b
Anil panthi <apanthi21@gmail.com>
2024-12-03 03:01:15 +0545

    Checking database installation

--- api/index.php
+++ api/index.php
@@ -1,60 +1,63 @@
 <?php
+
+var_dump('Hellows');
+
 // header('Content-Type: application/json');
 // header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 // header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-require_once __DIR__ . '/config/config.php';
+// require_once __DIR__ . '/config/config.php';
 
-require_once __DIR__ . '/models/DatabaseInstaller.php';
-require_once __DIR__ . '/models/Database.php';
-require_once __DIR__ . '/models/License.php';
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/models/DatabaseInstaller.php';
+// require_once __DIR__ . '/models/Database.php';
+// require_once __DIR__ . '/models/License.php';
+// require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/helpers/JwtHelper.php';
-
-
-require_once __DIR__ .'/controllers/RestController.php';
-require_once __DIR__ . '/controllers/LicenseController.php';
-require_once __DIR__ . '/routes/index.php';
-
-$installer = new DatabaseInstaller();
-
-var_dump($installer);
-
-if (!$installer->isInstalled()) {
-    if ($installer->install()) {
-        echo json_encode([
-            'status' => 'success',
-            'message' => 'Database installed successfully',
-        ]);
-    } else {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Database installation failed',
-        ]);
-        exit;
-    }
-} 
+// require_once __DIR__ . '/helpers/JwtHelper.php';
 
 
+// require_once __DIR__ .'/controllers/RestController.php';
+// require_once __DIR__ . '/controllers/LicenseController.php';
+// require_once __DIR__ . '/routes/index.php';
+
+// $installer = new DatabaseInstaller();
+
+// var_dump($installer)
+
+// if (!$installer->isInstalled()) {
+//     if ($installer->install()) {
+//         echo json_encode([
+//             'status' => 'success',
+//             'message' => 'Database installed successfully',
+//         ]);
+//     } else {
+//         echo json_encode([
+//             'status' => 'error',
+//             'message' => 'Database installation failed',
+//         ]);
+//         exit;
+//     }
+// } 
 
 
-$basePath = '/api';
 
-var_dump($basePath);
 
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$requestUri = str_replace($basePath, '', $requestUri);
-$requestUri = rtrim($requestUri, '/');
+// $basePath = '/api';
 
-var_dump($requestUri);
+// var_dump($basePath);
 
-$requestMethod = $_SERVER['REQUEST_METHOD'];
+// $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $requestUri = str_replace($basePath, '', $requestUri);
+// $requestUri = rtrim($requestUri, '/');
 
-if ($requestMethod === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
+// var_dump($requestUri);
 
-$router->dispatch($requestMethod, $requestUri);
+// $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// if ($requestMethod === 'OPTIONS') {
+//     http_response_code(200);
+//     exit;
+// }
+
+// $router->dispatch($requestMethod, $requestUri);
