1f6200935d8271a5b2a40e9e90065b20058a1dfa
Anil panthi <apanthi21@gmail.com>
2024-12-03 03:03:52 +0545

    config file added

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ var_dump('Hellows');
 // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 // header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-// require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/config/config.php';
 
 require_once __DIR__ . '/models/DatabaseInstaller.php';
 require_once __DIR__ . '/models/Database.php';
