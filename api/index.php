f71ed9f23bb8a09b25d2108b62bde2746bfa26c0
Anil panthi <apanthi21@gmail.com>
2024-12-03 03:25:28 +0545

    uncommented all codes from index.php coz databse is installed now

--- api/index.php
+++ api/index.php
@@ -1,25 +1,25 @@
 <?php
 
-var_dump('Hellows');
 
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-// header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 require_once __DIR__ . '/config/config.php';
 
 require_once __DIR__ . '/models/DatabaseInstaller.php';
 require_once __DIR__ . '/models/Database.php';
 require_once __DIR__ . '/models/License.php';
-// require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-// require_once __DIR__ . '/helpers/JwtHelper.php';
+require_once __DIR__ . '/helpers/JwtHelper.php';
 
 
-// require_once __DIR__ .'/controllers/RestController.php';
-// require_once __DIR__ . '/controllers/LicenseController.php';
-// require_once __DIR__ . '/routes/index.php';
+require_once __DIR__ .'/controllers/RestController.php';
+require_once __DIR__ . '/controllers/LicenseController.php';
+require_once __DIR__ . '/routes/index.php';
 
 $installer = new DatabaseInstaller();
 
@@ -43,21 +43,21 @@ if (!$installer->isInstalled()) {
 
 
 
-// $basePath = '/api';
+$basePath = '/api';
 
 // var_dump($basePath);
 
-// $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-// $requestUri = str_replace($basePath, '', $requestUri);
-// $requestUri = rtrim($requestUri, '/');
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestUri = str_replace($basePath, '', $requestUri);
+$requestUri = rtrim($requestUri, '/');
 
-// var_dump($requestUri);
+var_dump($requestUri);
 
-// $requestMethod = $_SERVER['REQUEST_METHOD'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-// if ($requestMethod === 'OPTIONS') {
-//     http_response_code(200);
-//     exit;
-// }
+if ($requestMethod === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
-// $router->dispatch($requestMethod, $requestUri);
+$router->dispatch($requestMethod, $requestUri);
