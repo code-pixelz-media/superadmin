b326b32bef1cbf3871b58a0560e210b02f376a39
Anil panthi <apanthi21@gmail.com>
2024-12-03 12:49:36 +0545

    commented some of the index.php code

--- api/index.php
+++ api/index.php
@@ -9,35 +9,35 @@
 
 require_once __DIR__ . '/config/config.php';
 
-require_once __DIR__ . '/models/DatabaseInstaller.php';
-require_once __DIR__ . '/models/Database.php';
-require_once __DIR__ . '/models/License.php';
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/models/DatabaseInstaller.php';
+// require_once __DIR__ . '/models/Database.php';
+// require_once __DIR__ . '/models/License.php';
+// require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/helpers/JwtHelper.php';
-
-
-require_once __DIR__ .'/controllers/RestController.php';
-require_once __DIR__ . '/controllers/LicenseController.php';
-require_once __DIR__ . '/routes/index.php';
-
-$installer = new DatabaseInstaller();
+// require_once __DIR__ . '/helpers/JwtHelper.php';
 
 
-if (!$installer->isInstalled()) {
-    if ($installer->install()) {
-        echo json_encode([
-            'status' => 'success',
-            'message' => 'Database installed successfully',
-        ]);
-    } else {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Database installation failed',
-        ]);
-        exit;
-    }
-} 
+// require_once __DIR__ .'/controllers/RestController.php';
+// require_once __DIR__ . '/controllers/LicenseController.php';
+// require_once __DIR__ . '/routes/index.php';
+
+// $installer = new DatabaseInstaller();
+
+
+// if (!$installer->isInstalled()) {
+//     if ($installer->install()) {
+//         echo json_encode([
+//             'status' => 'success',
+//             'message' => 'Database installed successfully',
+//         ]);
+//     } else {
+//         echo json_encode([
+//             'status' => 'error',
+//             'message' => 'Database installation failed',
+//         ]);
+//         exit;
+//     }
+// } 
 
 
 
@@ -53,9 +53,9 @@ $requestUri = rtrim($requestUri, '/');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($requestMethod === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
+// if ($requestMethod === 'OPTIONS') {
+//     http_response_code(200);
+//     exit;
+// }
 
-$router->dispatch($requestMethod, $requestUri);
+// $router->dispatch($requestMethod, $requestUri);
